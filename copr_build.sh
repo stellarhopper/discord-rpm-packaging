e27e94dfbebe7afa1af82165031f96d21f64ea5b
Vishal Verma <vishal@stellar.sh>
2017-11-28 23:47:07 -0700

    disable the actual copr build

--- copr_build.sh
+++ copr_build.sh
@@ -2,6 +2,7 @@
 
 url="https://discordapp.com/api/download/canary?platform=linux&format=tar.gz"
 discord_dir="DiscordCanary"
+rel=27
 
 # get the latest tgz
 url2=$(curl -s "$url" | grep "a href=" | cut -d= -f2 | cut -d'"' -f2)
@@ -18,10 +19,10 @@ tar czf "$file" "$discord_dir"
 rm -rf "$discord_dir"
 
 # build srpm
-fedpkg --release f25 --module-name discord-canary mockbuild
+fedpkg --release f$rel --module-name discord-canary mockbuild
 
 # push to copr
 ver="${file##*-}"
 ver="${ver%.tar.gz}"
-copr-cli build discord-canary ./results*/"$ver"/1.fc25/*.src.rpm
+#copr-cli build discord-canary ./results*/"$ver"/1.fc$rel/*.src.rpm
 rm ./*.tar.gz

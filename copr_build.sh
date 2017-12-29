74a32218695e06c299e8073c469b1e3bb1dc3066
Vishal Verma <vishal@stellar.sh>
2017-12-28 20:05:48 -0700

    build script: detect version and auto update spec

--- copr_build.sh
+++ copr_build.sh
@@ -8,9 +8,21 @@ rel=27
 url2=$(curl -s "$url" | grep "a href=" | cut -d= -f2 | cut -d'"' -f2)
 file=${url2##*/}
 [ -s "$file" ] || curl -s -O "$url2"
+ver="${file##*-}"
+ver="${ver%.tar.gz}"
+
+# prepare spec, push
+git reset --hard HEAD
+git pull -q
+
+# If spec already has the currently downloaded veresion, then it may be a rebuild
+if ! grep -q $ver discord-canary.spec; then
+	sed -i "s/$(grep "Version:" discord-canary.spec | cut -d' ' -f9-)/$ver/" discord-canary.spec
+	git commit -am "Update to $ver (Automated)"
+	git push
+fi
 
 # add the spec and desktop files
-git pull
 tar xf "$file"
 cp ./*.spec ./*.desktop "$discord_dir"
 
@@ -22,7 +34,5 @@ rm -rf "$discord_dir"
 fedpkg --release f$rel --module-name discord-canary mockbuild
 
 # push to copr
-ver="${file##*-}"
-ver="${ver%.tar.gz}"
-#copr-cli build discord-canary ./results*/"$ver"/1.fc$rel/*.src.rpm
+# copr-cli build discord-canary ./results*/"$ver"/1.fc$rel/*.src.rpm
 rm ./*.tar.gz
